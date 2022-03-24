99074f4278f8b86e98f46861afb6268dc16b1b98
ErikaMessias <majmrs@gmail.com>
2022-03-24 09:55:21 -0300

    first commit

---
+++ pages/config/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cont">
+    <headers />
+        <div class="page">
+          <div class="content-block">
+            <!-- <h1>Configurações</h1> -->
+            <div class="cards">
+              <div class="card-one c">
+                <h1>Cadastrar Usuario</h1>
+                <img src="/sucata.png">
+                <NuxtLink class="btn-ir" to="/cadastro_usuario">Selecionar</NuxtLink>                
+              </div>
+
+              <div class="card-two c">
+                <h1>Consultar Usuario</h1>
+                <img src="/produto.png">
+                <NuxtLink class="btn-ir" to="/consulta_usuario">Selecionar</NuxtLink>                
+              </div>
+            </div>
+          </div>
+        </div>
+    <footers />
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss" scoped>
+  @import '@/pages/transacoes/transacoes.scss'
+</style>
